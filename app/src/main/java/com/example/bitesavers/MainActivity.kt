06c20936f84324c0edaf7f2e9a76437e3a6a7e53
jason5292 <jyanextra@gmail.com>
2026-08-04 10:17:33 +0800

    Testing Push, Changed name

--- app/src/main/java/com/example/bitesavers/MainActivity.kt
+++ app/src/main/java/com/example/bitesavers/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             BiteSaversTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Jyan Lim",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
